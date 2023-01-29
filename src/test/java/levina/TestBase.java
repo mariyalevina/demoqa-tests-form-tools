55492535a8dca17f7c80520a077b60e58d229916
Mariya <mariya.levina@simbirsoft.com>
2023-01-29 22:50:42 +0300

    code refactoring

--- src/test/java/levina/TestBase.java
+++ src/test/java/levina/TestBase.java
@@ -14,15 +14,15 @@ public class TestBase {
     static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         String baseUrl = System.getProperty("baseUrl");
+        String browserSize = System.getProperty("browserSize");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
-        Configuration.browserSize = "2100x1080";
+        Configuration.browserSize = browserSize;
         Configuration.baseUrl = baseUrl;
-       // Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
