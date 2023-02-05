65d902f02f83d59959fe9397d088f3f4fd7b4fbb
Mariya <mariya.levina@simbirsoft.com>
2023-02-05 16:19:59 +0300

    added negative test

--- src/test/java/levina/tests/StudentRegistrationFormTests.java
+++ src/test/java/levina/tests/StudentRegistrationFormTests.java
@@ -58,4 +58,36 @@ public class StudentRegistrationFormTests extends TestBase {
                 text("Female"), text("1234567890"), text("08 November,2022"), text("Chemistry"), text("Sports"),
                 text("123.png"), text("Ulyanovsk, st. Kamyshinskaya 105"), text("NCR Delhi"));
     }
+
+    @Test
+    void fillFormTestNegative() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Mariya");
+        $("#lastName").setValue("Levina");
+        $("#userEmail").setValue("mariya.levina@simbirsoft.com");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("1234567890");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("November");
+        $(".react-datepicker__year-select").selectOption("2022");
+        $(".react-datepicker__day--008:not(.react-datepicker__day--outside-month)").click();
+
+        $("#subjectsInput").setValue("Chemistry").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/123.png");
+
+        $("#currentAddress").setValue("Ulyanovsk, st. Kamyshinskaya 105");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+
+        $("#submit").click();
+
+        $(".modal-title").shouldHave(text("111111111111111"));
+        $(".table-responsive").shouldHave(text("Mariya Levina"), text("mariya.levina@simbirsoft.com"),
+                text("Female"), text("1234567890"), text("08 November,2022"), text("Chemistry"), text("Sports"),
+                text("123.png"), text("Ulyanovsk, st. Kamyshinskaya 105"), text("NCR Delhi"));
+    }
 }
